fdf38a154563171a0572d27a56823ee9079ccf43
mikayel.nahapetyan <mikayel.nahapetyan@softconstruct.com>
2023-10-03 17:23:35 +0400

    HTTp_method_2

--- cypress/e2e/HTTP_Methods_Part_2.cy.js
+++ cypress/e2e/HTTP_Methods_Part_2.cy.js
@@ -2,7 +2,7 @@
 
 
 describe('api testing', ()=>{
-   
+   //every time change email addres
     // it("Apporch1- Hard coded json object", ()=>{
     //     const requestBody={
     //         tourist_name:"Mike25",
@@ -48,29 +48,29 @@ describe('api testing', ()=>{
     })
 
 
+//every time change email addres
+    // it("Apporch3- using Fixture", ()=>{
+    //     cy.fixture('tourist').then((data)=>{
 
-    it("Apporch3- using Fixture", ()=>{
-        cy.fixture('tourist').then((data)=>{
+    //         const requestBody =data
+    //         cy.request(
+    //             {
+    //                 method: 'POST',
+    //                 url:"http://restapi.adequateshop.com/api/Tourist",
+    //                 body: requestBody
+    //             })
+    //             .then((response) =>{
+    //                 expect(response.status).to.eq(201)
+    //                 expect(response.body.tourist_name).to.eq(requestBody.tourist_name)
+    //                 expect(response.body.tourist_email).to.eq(requestBody.tourist_email)
+    //                 expect(response.body.tourist_location).to.eq(requestBody.tourist_location)
 
-            const requestBody =data
-            cy.request(
-                {
-                    method: 'POST',
-                    url:"http://restapi.adequateshop.com/api/Tourist",
-                    body: requestBody
-                })
-                .then((response) =>{
-                    expect(response.status).to.eq(201)
-                    expect(response.body.tourist_name).to.eq(requestBody.tourist_name)
-                    expect(response.body.tourist_email).to.eq(requestBody.tourist_email)
-                    expect(response.body.tourist_location).to.eq(requestBody.tourist_location)
+    //                 expect(response.body).has.property('tourist_email', requestBody.tourist_email)
+    //                 expect(response.body).to.have.property('tourist_email', requestBody.tourist_email)
+    //             })
 
-                    expect(response.body).has.property('tourist_email', requestBody.tourist_email)
-                    expect(response.body).to.have.property('tourist_email', requestBody.tourist_email)
-                })
-
-        })
+    //     })
 
         
-    })
+    // })
 })
