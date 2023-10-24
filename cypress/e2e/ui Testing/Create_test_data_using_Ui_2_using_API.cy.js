1f862ff0c622e50024c40d124137af894979e2f7
mikayel.nahapetyan <mikayel.nahapetyan@softconstruct.com>
2023-10-24 19:31:06 +0400

    3. Stubbing our todos using the Intercept command

--- cypress/e2e/ui Testing/Create_test_data_using_Ui_2_using_API.cy.js
+++ cypress/e2e/ui Testing/Create_test_data_using_Ui_2_using_API.cy.js
@@ -4,29 +4,55 @@ describe('filter functionality test case', () => {
     // Mock
 
     before(() => {
-        cy.addDummyTodos();
+        //cy.addDummyTodos();// from support commands
+        
+        cy.intercept({
+            method: 'GET',
+            url:'http://localhost:8080/todos'
+        },{
+            body:[
+                {
+                    "name" : "learn cypress",
+                    "isComplete": false
+                },
+                {
+                    "name" : "build framework",
+                    "isComplete": true
+                },
+                {
+                    "name" : "shopping",
+                    "isComplete": false
+                },
+                {
+                    "name" : "drink coffe",
+                    "isComplete": true
+                }
+            ]
+        })
+        
+        
         cy.visit('http://localhost:3000');
     });
 
 
 
-    // it('should filter the completed todos correctly-2111', () => {
-    //     cy.contains('Complete').click();
-    //     cy.url().should('contain', '/complete');
-    //     cy.get('.todo-checkbox').each(element => {
-    //         cy.wrap(element).should('be.checked');
-    //     });
-    // });
+    it('should filter the completed todos correctly-2111', () => {
+        cy.contains('Complete').click();
+        cy.url().should('contain', '/complete');
+        cy.get('.todo-checkbox').each(element => {
+            cy.wrap(element).should('be.checked');
+        });
+    });
 
     // erkrork@ chi ashxatum iriair het uxxel 
 
-    it('should filter the active todos correctly-31111', () => {
-        cy.get('.footer > :nth-child(2)').click();
-       cy.url().should('contain', '/active');
-        cy.get('.todo-checkbox').each(element => {
-            cy.wrap(element).should('not.be.checked');
-        });
-    });
+    // it('should filter the active todos correctly-31111', () => {
+    //     cy.get('.footer > :nth-child(2)').click();
+    //    cy.url().should('contain', '/active');
+    //     cy.get('.todo-checkbox').each(element => {
+    //         cy.wrap(element).should('not.be.checked');
+    //     });
+    // });
 
     after(() => {
         cy.get('body').then($el => {
